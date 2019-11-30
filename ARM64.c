beb717c9401ee287da9d2dc1975db93486e9c476
Grigorii <g.bykovskii@spb.mcart.ru>
2019-11-30 07:14:53 +0300

    Right initial

---
+++ ARM64.c
@@ -0,0 +1,25 @@
+﻿// ARM64.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+unsigned A, B, Q, Z, N;	
+
+int main()
+{
+	printf("Input dividend: ");
+	scanf("%u", &A);
+	printf("Input divider: ");
+	scanf("%u", &B);
+	printf("A= %u, B= %u\n", A, B);
+
+	asm(
+		"adr x4, A\n"
+		"ldr x0, [x4]\n"
+		"adr x4, B\n"
+		"ldr x1, [x4]\n"
+	);
+
+	return 0;
+}
+
