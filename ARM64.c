b2e86b226dcff42c834a765eef0bab53d1593eab
Grigorii <g.bykovskii@spb.mcart.ru>
2019-12-06 20:46:27 +0300

    New labels fix

--- ARM64.c
+++ ARM64.c
@@ -27,7 +27,6 @@ int main()
         "asr x2, x0, x4\n" //
         "mov x3, #0\n"
         "sub x3, x2, x1\n"
-        "cmp x3, #0\n"
         "bmi more\n"
         "more:\n"
         "mov x8, #0\n"
